fdc3b8e050cfff47be775794f58ad7ae9a537d47
Treisy-dev <vip.protop2004@gmail.com>
2024-03-19 11:06:18 +0300

    remove force unwrap in extension method

--- DiaCare/DiaCare/Extensions/UIImageExtensions.swift
+++ DiaCare/DiaCare/Extensions/UIImageExtensions.swift
@@ -12,8 +12,8 @@ extension UIImage {
     func resizeImage(newSize: CGSize) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
         self.draw(in: CGRect(origin: .zero, size: newSize))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else { return self }
         UIGraphicsEndImageContext()
-        return newImage!
+        return newImage
     }
 }
